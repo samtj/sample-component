b1038e38378e27afb8d6df8cbebae4f49ec541eb
Samuel T <samtjok@gmail.com>
2015-02-10 17:38:02 -0600

    added missing files

---
+++ app/controllers/sample.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:SampleCtrl
+ * @description
+ * # AddedCtrl
+ * Controller of the mytodoApp
+ */
+angular.module('SampleModule',['btford.dragon-drop']).
+    controller('SampleCtrl', function ($scope) {
+        $scope.things = ['one', 'two'];
+        $scope.otherThings = [];
+    });
+/*
+    angular.module('mytodoApp')
+    .controller('AboutCtrl', function ($scope) {
+#    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });*/
